0e9bb155d0759d63f2662f3be96a554dd0f008a4
Anith Vishwanath <11884278+anithvishwanath@users.noreply.github.com>
2024-11-30 20:28:55 +0530

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -10,8 +10,8 @@ const settings = {
 
 const sketch = () => {
   const margin = 120;
-  const cols = 16;
-  const rows = cols;
+  const cols = 14;
+  const rows = 8;
 
   return ({ context, width, height, playhead }) => {
     context.fillStyle = 'hsl(0, 0%, 98%)';
@@ -20,26 +20,28 @@ const sketch = () => {
     const tileWidth = (width - 2 * margin) / cols;
     const tileHeight = (height - 2 * margin) / rows;
 
-    const animationSpeed = 3;
+    const animationSpeed = 2;
+    const goldenAngle = 137.5 * (Math.PI / 180);
 
     for (let i = 0; i < cols; i++) {
       for (let j = 0; j < rows; j++) {
         const tilePosX = margin + tileWidth * i;
         const tilePosY = margin + tileHeight * j;
 
-        context.strokeStyle = 'grey';
-        context.strokeRect(tilePosX, tilePosY, tileWidth, tileHeight);
+        // context.strokeStyle = 'grey';
+        // context.strokeRect(tilePosX, tilePosY, tileWidth, tileHeight);
 
         context.save();
-        context.strokeStyle = 'hsl(0, 0%, 9%)';
-        context.lineWidth = 8;
+        context.strokeStyle = 'hsl(240, 100%, 50%)';
+        context.lineWidth = 12;
+        context.lineCap = 'round';
 
         context.translate(tilePosX + tileWidth / 2, tilePosY + tileHeight / 2);
-        context.rotate(Math.sin(playhead * Math.PI * 2 * animationSpeed + i * 0.2 - j * 0.2) * Math.PI / 2);
+        context.rotate(Math.sin(playhead * Math.PI * 2 * animationSpeed + i * 0.2 + j * 0.2) * Math.PI / 2);
 
         context.beginPath();
-        context.moveTo(0, 0);
-        context.lineTo(-tileWidth / 2, tileWidth / 2);
+        context.moveTo(-tileWidth / 4, 0);
+        context.lineTo(tileWidth / 4, 0);
         // context.arc(0, 0, tileWidth / 3, 0, Math.PI * 2);
 
         context.stroke();
